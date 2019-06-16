fdaa2d6f92ae78501f49ef88a17b5db526198af7
Cnfc <stdashkov@mail.ru>
2019-06-16 16:28:26 +0200

    add base structure and HOC layout (header & footer)

---
+++ src/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Layout from './HOC/Layout';
+
+import Home from './components/Home/Home';
+// import RegisterLogin from './components/Register_login';
+// import Register from './components/Register_login/register';
+// import UserDashboard from './components/User';
+
+
+const Routes = () => {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+
+      </Switch>
+    </Layout>
+    
+  )
+}
+
+export default Routes;
---
+++ src/components/Header_footer/Footer/index.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faCompass from '@fortawesome/fontawesome-free-solid/faCompass';
+import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
+import faClock from '@fortawesome/fontawesome-free-solid/faClock';
+import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
+
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <footer className='bck_b_dark'>
+        <div className='container'>
+          <div className="logo">
+            Houses
+          </div>
+          <div className='wrapper'>
+            <div className='left'>
+              <h2>Contact information</h2>
+              <div className='business_nfo'>
+              {/* ADDRESS */}
+                <div className='tag'>
+                  <FontAwesomeIcon
+                    icon={faCompass}
+                    className='icon'
+                  />
+                  <div className='nfo'>
+                    <div>Address</div>
+                    <div>Some adress</div>
+                  </div>
+                </div>
+
+                  {/* Phone*/}
+                <div className='tag'>
+                  <FontAwesomeIcon
+                    icon={faPhone}
+                    className='icon'
+                  />
+                  <div className='nfo'>
+                    <div>faPhone</div>
+                    <div>4254-564-435</div>
+                  </div>
+                </div>
+
+
+                {/* Clock */}
+                <div className='tag'>
+                  <FontAwesomeIcon
+                    icon={faClock}
+                    className='icon'
+                  />
+                  <div className='nfo'>
+                    <div>Working house</div>
+                    <div>7mon-Sun/ 9am-8pm</div>
+                  </div>
+                </div>
+
+                {/* EMAIL */}
+                <div className='tag'>
+                  <FontAwesomeIcon
+                    icon={faEnvelope}
+                    className='icon'
+                  />
+                  <div className='nfo'>
+                    <div>Email</div>
+                    <div>some@mail.com</div>
+                  </div>
+                </div>
+                {/* Some adding zone*/}
+                <div className='tag'>
+                  <FontAwesomeIcon
+                    className='icon'
+                  />
+                  <div className='nfo'>
+                    <div>faZone</div>
+                    <div>Some</div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+            <div className='left'>
+              <h2>Be first to know</h2>
+              <div>
+                Get all the latest iformation to some@mail.com
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
