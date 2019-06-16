b58696728c2a3dc5af8bf3522be681aede5e00da
Cnfc <stdashkov@mail.ru>
2019-06-16 17:17:06 +0200

    add links header

--- src/components/Header_footer/Header/index.js
+++ src/components/Header_footer/Header/index.js
@@ -1,11 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 class Header extends Component {
+
+  state = {
+    page: [
+      {
+        name: 'Home',
+        linkTo: '/',
+        public: true
+      },
+      {
+        name: 'About',
+        linkTo: '/about',
+        public: true
+      },
+      {
+        name: 'Tour',
+        linkTo: '/tour',
+        public: true
+      },
+      {
+        name: 'Gallery',
+        linkTo: '/gallery',
+        public: true
+      },
+    ]
+  }
+
+  defaultLinks = (item ,i) => (
+    <Link to={item.linkTo} key={i}>
+      {item.name}
+    </Link>
+  )
+
+  showlinks = (type) => {
+    let list = [];
+
+    type.forEach((item) => {
+      if(item.public === true){
+        list.push(item)
+      }
+    } )
+    
+
+    return list.map((item ,i) => {
+      return this.defaultLinks(item, i);
+    })
+  }
 
   render() {
     return (
       <div>
-        Header
+        {this.showlinks(this.state.page)}
       </div>
     );
   }
